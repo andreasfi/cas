3dfc4f7c23aebb8815eba451f1f4ddcf4ccb3653
andreas <andreasfi@me.com>
2016-09-27 10:55:19 +0200

    Details view and controller

--- views/sorties/details.php
+++ views/sorties/details.php
@@ -5,3 +5,47 @@
  * Date: 27/09/2016
  * Time: 08:48
  */
+
+include_once  'views/header.inc'; ?>
+
+    <div class="content">
+        <div class="container">
+            <div class="col-lg-6">
+                <h2 style="margin-top: 6px;">Titre rando</h2>
+            </div>
+            <div class="col-lg-6">
+
+                <a  href="#" class="btn btn-danger btn-large pull-right">Je veux participer <i class="fa fa-angle-double-right"></i></a>
+                <a  href="#" class="btn btn-info btn-large pull-right">Trouver itinéraire <i class="fa fa-angle-double-right"></i></a>
+            </div>
+
+            <br>
+            <img class="center-block" src="http://placehold.it/1140x250">
+            <div class="row features-two">
+
+                <div class="col-md-12 col-sm-12">
+                    <div class="f-block bblue">
+                        <a href="#"><i class="fa fa-briefcase"></i></a>
+                        <a href="#"><h3>La Randoooo</h3></a>
+                        <p>
+                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                            027 / 000 00 00
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-12">
+                <img class="center-block" src="http://placehold.it/600x150">
+            </div>
+            <div class="col-lg-12">
+                <img class="center-block" src="http://placehold.it/300x150">
+            </div>
+
+
+
+        </div>
+    </div>
+
+<?php
+include_once 'views/footer.inc';
