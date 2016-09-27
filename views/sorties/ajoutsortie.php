dadc78742a455f5bc505611e0df887e10ab97b92
calixtemayoraz <calixtemayoraz@gmail.com>
2016-09-27 15:55:00 +0200

    finished form, working on google maps

--- views/sorties/ajoutsortie.php
+++ views/sorties/ajoutsortie.php
@@ -7,45 +7,70 @@
  */
 
 include_once  'views/header.inc'; ?>
+<style>
+	.formgroup{
+		margin-bottom: 25px;
+	}
+	
+	.formgroup p{
+		font-size: 15pt !important;
+		
+	}
+</style>
+<div class="container">
+	<div class="row">
+		<div class="col-md-3 col-xs-1"></div>
+		<div class="col-md-6 col-xs-10 text-center">
+			<form>
+				<div class="formgroup">
+					<p>Titre de la randonnée</p>
+					<input name="title" type="text" required>
+				</div>
+				<div class="formgroup">
+					<p>Nombre de personnes max</p>
+					<input name="maxParticipants" type="number" min="1" required>
+				</div>
+				<div class="formgroup">
+					<p>Difficulté</p>
+					<select name="difficulty" required>
+						<option value="1">Débutant</option>
+						<option value="2">Modéré</option>
+						<option value="3">Avancé</option>
+						<option value="4">Très Avancé</option>
+						<option value="5">Professionnel</option>
+					</select>
+				</div>
+				<div class="formgroup">
+					<p>Date/Heure de début</p>
+					<input id="startDate" name="startDate" type="date" required> 
+					<input name="startTime" type="time" required>
+				</div>
+				<div class="formgroup">
+					<p>Date/Heure de fin</p>
+					<input id="#endDate" name="endDate" type="date" required>
+					<input name="endTime" type="time">
+				</div>
+				<div class="formgroup">
+					<p>Description</p>
+					<textarea rows="6" cols="50" name="description"></textarea>
+				</div>
+				<div id="#map" style="width:100%; height:250px;"></div>
+			</form>
+		</div>
+		<div class="col-ms-3 col-xs-1"></div>
+	</div>
+</div>
+<script>
+	var map;
+	function initMap() {
+		map = new google.maps.Map(document.getElementById('map'), {
+			center: {lat: -34.397, lng: 150.644},
+			zoom: 8
+		});
+	}
+</script>
 
-    <div class="content">
-        <div class="container">
-            <div class="col-lg-6">
-                <h2 style="margin-top: 6px;">Titre rando</h2>
-            </div>
-            <div class="col-lg-6">
-
-                <a  href="#" class="btn btn-danger btn-large pull-right">Je veux participer <i class="fa fa-angle-double-right"></i></a>
-                <a  href="#" class="btn btn-info btn-large pull-right">Trouver itinéraire <i class="fa fa-angle-double-right"></i></a>
-            </div>
-
-            <br>
-            <img class="center-block" src="http://placehold.it/1140x250">
-            <div class="row features-two">
-
-                <div class="col-md-12 col-sm-12">
-                    <div class="f-block bblue">
-                        <a href="#"><i class="fa fa-briefcase"></i></a>
-                        <a href="#"><h3>La Randoooo</h3></a>
-                        <p>
-                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                            027 / 000 00 00
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-12">
-                <img class="center-block" src="http://placehold.it/600x150">
-            </div>
-            <div class="col-lg-12">
-                <img class="center-block" src="http://placehold.it/300x150">
-            </div>
-
-
-
-        </div>
-    </div>
-
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAR9yaAK5avHng7msvuDKUMtWGa_hDnAVA&callback=initMap"
+    async defer></script>
 <?php
 include_once 'views/footer.inc';
