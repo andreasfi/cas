b89a12c3c83d84958fe6e378eadf59e1e4519330
Calixte Mayoraz <calixtemayoraz@gmail.com>
2016-10-27 14:10:52 +0200

    loading image
    image loaded by searching for highest point, reverse geocoding, and
    searching in a custom search engine

--- views/sorties/proximite.php
+++ views/sorties/proximite.php
@@ -43,8 +43,8 @@ include_once ROOT_DIR.'views/header.inc';
 		infowindow = new google.maps.InfoWindow({maxWidth: 300});
 		
 		for (var point in startPoints){
-			//check if event has a trail mapped, and if it is in the future
-			if(startPoints[point].path != null && Date.parse(startPoints[point].start_date) > Date.parse(new Date)){
+			//check if event has a trail mapped
+			if(startPoints[point].path != null){
 				addPoint(startPoints[point]);	
 			}
 		}
@@ -68,7 +68,7 @@ include_once ROOT_DIR.'views/header.inc';
 		var output = '<h3>' + decodeHTML(event.title) + '</h3>';
 		output += '<p>' + event.description + '</p>';
 		output += '<a href="details/' + event.id +'"> Plus d\'informations</a>'; 
-		output += '<img src="https://maps.googleapis.com/maps/api/streetview?size=400x400&location='+event.path[0].lat+','+event.path[0].lng + '&fov=90&heading=235&pitch=10&key=AIzaSyAu8ca8nVd7JUvLV4RuGQJZC12tQCBVJgE">'
+		calculateElevation(event.path)
 		return output;
 	}
 	
@@ -78,11 +78,62 @@ include_once ROOT_DIR.'views/header.inc';
     	return txt.value;
 	}
 	
+	function calculateElevation(points){
+		var max = {lat: 0, lng:0};
+		var elevator = new google.maps.ElevationService();
+		if(points.length > 2){
+			elevator.getElevationAlongPath({
+				'path': points,
+				'samples': 256
+			}, calculateSummit);
+		}else{
+			elevator.getElevationForLocations({
+				'locations': [points[0]]
+			}, calculateSummit);
+		}
+	}
+	
+	function calculateSummit(elevations, status){
+		//find maximum
+		var max = 0;
+		for(var i = 0 ; i < elevations.length; i++){
+			if(elevations[i].elevation > elevations[max].elevation)
+				max = i;
+		}
+		
+		setSummitPicture(elevations[max]);
+	}
+	
+	function setSummitPicture(point){
+		
+		//send coordinates of summit to google to find the name
+		var service = new google.maps.places.PlacesService(map);
+		service.nearbySearch({
+			location : point.location,
+			radius : 500,
+			type : ['natural_feature']
+		}, findAndDisplayImage);
+	}
+	
+	function findAndDisplayImage(results, status){
+		var regex = new RegExp('" "', 'g');
+		var searchTerm = (results[0].name).replace(regex, '+');
+		$.get('https://www.googleapis.com/customsearch/v1?q='+ searchTerm +'&cx=006299086801710193067%3A5_sbtpwr7km&searchType=image&key=AIzaSyCfHSiXZQseH8j-pPHb9PiWwvGvpOUSDGw', function(data, status){
+			var image = '<a href="'+ data.items[0].link+'"><img style="textalign:center" src="'+ data.items[0].link +'" width="200"></a>';
+			infowindow.setContent(infowindow.getContent() + image);
+			infowindow.setContent(infowindow.getContent() + results[0].name);
+			
+		});
+
+	}
 	
 	
-	//street view API Key AIzaSyAu8ca8nVd7JUvLV4RuGQJZC12tQCBVJgE
+	
+	//street view API Key :AIzaSyAu8ca8nVd7JUvLV4RuGQJZC12tQCBVJgE
+	//search engine id: 006299086801710193067:5_sbtpwr7km
+
 </script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCfHSiXZQseH8j-pPHb9PiWwvGvpOUSDGw&callback=initMap"
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCfHSiXZQseH8j-pPHb9PiWwvGvpOUSDGw&libraries=places&callback=initMap"
     async defer></script>
 <?php include_once ROOT_DIR.'views/footer.inc';
 ?>
