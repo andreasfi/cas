775ea255e91c45bdf415c5a32e7f4a14e327aa59
andreas <andreasfi@me.com>
2016-11-08 16:57:29 +0100

    Date correction

--- controllers/Class.sortiesController.php
+++ controllers/Class.sortiesController.php
@@ -246,8 +246,8 @@ class sortiesController extends Controller
         $this->vars['msg_err'] = isset($_SESSION['msg_err']) ? $_SESSION['msg_err'] : '';
         $this->vars['pageTitle'] = $result->getTitle();
         // Formate dates
-        $this->vars['endDate'] = date('d.m,y H:i',strtotime($result->getEndDate()));
-        $this->vars['pageMessage'] = date('y, H:i',strtotime($result->getStartDate()));
+        $this->vars['endDate'] = date('d.m.Y H:i',strtotime($result->getEndDate()));
+        $this->vars['pageMessage'] = date('d.m.Y, H:i',strtotime($result->getStartDate()));
 
         $this->vars['participating'] = $participating;
 
